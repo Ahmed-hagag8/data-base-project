70d293597df138cdc628e24c82636ff3fe7fc302
Ahmed-hagag8 <157177560+Ahmed-hagag8@users.noreply.github.com>
2024-04-09 12:31:11 +0200

    update-2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\studentsController;
+use App\Models\students;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('students',studentsController::class);
+
